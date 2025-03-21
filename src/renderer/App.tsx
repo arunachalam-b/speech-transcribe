d7bfe9f48d63dc52051073c73d8ea0a23328feb6
Abiraman K <abiramancit@gmail.com>
2025-03-21 09:54:23 +0530

    refactor(visualization): show audio visual effect based on live audio feed

--- src/renderer/App.tsx
+++ src/renderer/App.tsx
@@ -1,18 +1,16 @@
-import { useEffect, useMemo, useRef, useState } from 'react';
+import { useEffect, useState } from 'react';
 import { Route, MemoryRouter as Router, Routes } from 'react-router-dom';
+import { LiveAudioVisualizer } from 'react-audio-visualize';
+
 import './App.css';
 import useSpacebarHold from './hooks/useSpacebarHold';
 
-let mediaRecorder: any;
+// let mediaRecorder: any;
 let audioChunks: any[] = [];
+const waveLineColor = 'rgba(255, 255, 255, 0.75)';
 
 function Hello() {
-  const frequencyTemplete = useMemo(() => new Array(100).fill(1), []);
-
-  const freqIntervalRef = useRef<ReturnType<typeof setInterval> | null>(null);
-
-  const [frequencies, setFrequencies] =
-    useState<Array<number>>(frequencyTemplete);
+  const [mediaRecorder, setMediaRecorder] = useState<MediaRecorder>();
 
   const [recording, setRecording] = useState(false);
   // const [transcribedText, setTranscribedText] = useState('');
@@ -30,55 +28,22 @@ function Hello() {
   //   };
   // }, []);
 
-  function startVisualization() {
-    freqIntervalRef.current = setInterval(() => {
-      setFrequencies((prevFreq) => [
-        ...prevFreq,
-        Math.floor(Math.random() * (100 - 10) + 10),
-      ]);
-
-      const lastFreq = document.getElementById('scrollContainerId');
-
-      if (lastFreq) {
-        lastFreq.scrollBy(250, 0);
-      }
-    }, 50);
-  }
-
-  function stopVisualization() {
-    if (freqIntervalRef.current) {
-      clearInterval(freqIntervalRef.current);
-    }
-
-    setFrequencies(frequencyTemplete);
-  }
-
-  useEffect(() => {
-    if (recording) {
-      startVisualization();
-    }
-
-    if (!recording) {
-      stopVisualization();
-    }
-
-    // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, [recording]);
-
   async function startRecording() {
     console.log('Starting Recording... ');
     setRecording(true);
     try {
       const stream = await navigator.mediaDevices.getUserMedia({ audio: true });
-      mediaRecorder = new MediaRecorder(stream);
+      const mediaRecorderLocal = new MediaRecorder(stream);
 
-      mediaRecorder.ondataavailable = (event: any) => {
+      setMediaRecorder(mediaRecorderLocal);
+
+      mediaRecorderLocal.ondataavailable = (event: any) => {
         if (event.data.size > 0) {
           audioChunks.push(event.data);
         }
       };
 
-      mediaRecorder.onstop = async () => {
+      mediaRecorderLocal.onstop = async () => {
         const audioBlob = new Blob(audioChunks, { type: 'audio/wav' });
         const arrayBuffer = await audioBlob.arrayBuffer();
 
@@ -95,7 +60,7 @@ function Hello() {
         // audio.play();
       };
 
-      mediaRecorder.start();
+      mediaRecorderLocal.start();
     } catch (error) {
       console.error('Error accessing microphone:', error);
     }
@@ -105,7 +70,8 @@ function Hello() {
     if (mediaRecorder) {
       console.log('Stopping Recording... ');
       mediaRecorder.stop();
-      mediaRecorder = null;
+      // mediaRecorder = null;
+      setMediaRecorder(undefined);
       setRecording(false);
     }
   }
@@ -117,6 +83,7 @@ function Hello() {
     } else {
       stopRecording();
     }
+    // eslint-disable-next-line react-hooks/exhaustive-deps
   }, [isHoldingSpace]);
 
   return (
@@ -128,43 +95,27 @@ function Hello() {
         alignItems: 'center',
       }}
     >
-      <div
-        id="scrollContainerId"
-        style={{
-          display: 'flex',
-          whiteSpace: 'nowrap',
-          overflow: 'auto',
-          width: 400,
-          height: 120,
-          scrollbarWidth: 'none',
-          alignItems: 'center',
-          columnGap: 3,
-        }}
-      >
+      {mediaRecorder ? (
+        <LiveAudioVisualizer
+          mediaRecorder={mediaRecorder}
+          width={400}
+          height={100}
+          barColor={waveLineColor}
+        />
+      ) : (
         <div
-          id="scrollChildParent"
           style={{
+            width: 400,
+            height: 100,
             display: 'flex',
             alignItems: 'center',
-            columnGap: 3,
           }}
         >
-          {frequencies.map((freq, index) => (
-            <div
-              // eslint-disable-next-line react/no-array-index-key
-              key={index}
-              style={{
-                width: 5,
-                height: freq,
-                background: 'white',
-                opacity: 0.4,
-                borderRadius: 20,
-                display: 'inline-block',
-              }}
-            />
-          ))}
+          <span style={{ color: waveLineColor }}>
+            -----------------------------------------------------------------------
+          </span>
         </div>
-      </div>
+      )}
       <div>
         {!recording && (
           <button onClick={startRecording} type="button">
