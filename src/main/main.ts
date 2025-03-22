6443ebfbe6e1770b7131bb734e76d87e8f59a443
Abiraman K <abiramancit@gmail.com>
2025-03-22 20:00:51 +0530

    feat(app): remove menus in top

--- src/main/main.ts
+++ src/main/main.ts
@@ -163,6 +163,8 @@ const createWindow = async () => {
     },
   });
 
+  mainWindow.setMenuBarVisibility(false);
+
   mainWindow.loadURL(resolveHtmlPath('index.html'));
 
   mainWindow.on('ready-to-show', () => {
